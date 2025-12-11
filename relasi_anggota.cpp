3f23f64e45f321d57f53c7d4a9ce374383ebcc5c
radit1107 <raditfawwazgayatra@gmail.com>
2025-12-11 11:55:53 +0700

    update
    Added functions for managing member relationships with loans.

--- relasi_anggota.cpp
+++ relasi_anggota.cpp
@@ -1,9 +1,72 @@
 #include "list_anggota.h"
-#include "list_peminjaman.h"
+#include "list_peminjaman.h"   
+#include <iostream>
+using namespace std;
 
-void tambahRelasiAnggota(Anggota *parent, Peminjaman *child) {
-    RelasiAnggota *r = new RelasiAnggota;
-    r->child = child;
-    r->next = parent->relasi;
-    parent->relasi = r;
+RelasiAnggota* alokasiRelasi(Peminjaman *child) {
+    RelasiAnggota *R = new RelasiAnggota;
+    R->child = child;
+    R->next = NULL;
+    return R;
+}
+
+void insertRelasi(Anggota *A, Peminjaman *P) {
+    RelasiAnggota *R = alokasiRelasi(P);
+
+    R->next = A->relasi;
+    A->relasi = R;
+
+    cout << "Relasi berhasil ditambahkan ke anggota: " << A->nama << endl;
+}
+
+RelasiAnggota* findRelasi(Anggota *A, Peminjaman *P) {
+    RelasiAnggota *R = A->relasi;
+
+    while (R != NULL) {
+        if (R->child == P) return R;
+        R = R->next;
+    }
+    return NULL;
+}
+
+void deleteRelasi(Anggota *A, Peminjaman *P) {
+
+    if (A->relasi == NULL) return;
+
+    RelasiAnggota *R = A->relasi;
+
+    if (R->child == P) {
+        A->relasi = R->next;
+        delete R;
+        return;
+    }
+    RelasiAnggota *prev = R;
+    R = R->next;
+
+    while (R != NULL) {
+        if (R->child == P) {
+            prev->next = R->next;
+            delete R;
+            return;
+        }
+        prev = R;
+        R = R->next;
+    }
+}
+
+void printRelasiAnggota(Anggota *A) {
+    cout << "\n=== Daftar Relasi Peminjaman Anggota ===\n";
+    cout << "Anggota : " << A->nama << " (" << A->idAnggota << ")\n";
+
+    if (A->relasi == NULL) {
+        cout << "(Tidak ada peminjaman)\n";
+        return;
+    }
+
+    RelasiAnggota *R = A->relasi;
+    while (R != NULL) {
+        cout << "- Peminjaman pada alamat object: " << R->child << endl;
+        // nanti bisa kamu tambah detail Peminjaman
+        R = R->next;
+    }
 }
